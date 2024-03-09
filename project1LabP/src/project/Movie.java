8c5f16cd0296af4b83aa894633cad7eaf0d3ae61
rodrigo <rodrigo.bb@outlook.pt>
2024-03-09 17:27:58 +0000

    começar a classe movie e a classe videoclub

--- project1LabP/src/project/Movie.java
+++ project1LabP/src/project/Movie.java
@@ -1,4 +1,52 @@
 package project;
+
 public class Movie {
+	private String title;
+	private int year;
+	private int quantity;
+	private int[][] rentals;
+	private double price;
+	private double tax;
+	private String code;
 	
+	public Movie(String title, int year, int quantity, String rentals, double price, double tax) {
+		this.title = title;
+		this.year = year;
+		this.quantity = quantity;
+		this.rentals = rentalsParse(rentals);
+		this.tax = tax;
+		this.code = codeMaker(title);
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public int[][] getRentals() {
+		return rentals;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public double getTax() {
+		return tax;
+	}
+	
+	private int[][] rentalsParse(String rentals) {
+		
+	}
+	
+	private String codeMaker(String title) {
+		
+	}
 }
--- project1LabP/src/project/VideoClub.java
+++ project1LabP/src/project/VideoClub.java
@@ -1,4 +1,97 @@
 package project;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+
 public class VideoClub {
+	private int totalProfit;
+	private int totalRevenue;
+	private Movie[] filmes;
 	
+	public VideoClub(String fileName, int numberOfMovies)throws
+	FileNotFoundException {
+		String[][] stock = readStock(fileName, numberOfMovies);
+		filmes = new Movie[stock.length];
+		
+		for(int i = 0; i < stock.length; i++) {
+			filmes[i] = new Movie(stock[i][0], // title
+									Integer.parseInt(stock[i][1]), // year
+									Integer.parseInt(stock[i][2]), // quantity
+									stock[i][3], // rentals
+									Double.parseDouble(stock[i][4]), // price
+									Double.parseDouble(stock[i][5]));// tax
+		}
+	}
+	
+	public int getNumberOfMovies() {
+		int filmesTotais = 0;
+		
+		for(Movie filme : filmes) {
+			filmesTotais += filme.getQuantity();
+		}
+		
+		return filmesTotais;
+	}
+	
+	public int numberAvailableMovies() {
+		return filmes.length;
+	}
+	
+	public double getTotalRevenue() {
+		return totalRevenue;
+	}
+	
+	public double getTotalProfit() {
+		return totalProfit;
+	}
+	
+	public String filterByYear(int year) {
+		return "pixa";
+	}
+	
+	public String filterByPrice(double price) {
+		
+	}
+	
+	public String filterAvailableMovies() {
+		
+	}
+	
+	public String activityLog(String rentalsFileName) throws
+	FileNotFoundException{
+		
+	}
+	
+	public void updateStock(String fileName) throws
+	FileNotFoundException {
+		
+	}
+	
+	private String[][] readStock(String fileName, int numberOfMovies)throws
+	FileNotFoundException {
+		Scanner ler = new Scanner(new File("fileName"));
+		ler.nextLine(); //skippar cabealho
+		
+		int nLinhas = 0;
+		
+		while(ler.hasNext()) {
+			nLinhas++;
+			ler.nextLine();
+		}
+		
+		ler.close();
+		
+		Scanner ler2 = new Scanner(new File("fileName"));
+		ler.nextLine(); //skippar cabealho
+		
+		String[][] stock = new String[numberOfMovies <= nLinhas ? numberOfMovies : nLinhas][6]; // se o n de linhas for menor que numberOfMovies utiliza-se isso
+		
+		for(int i = 0; i <= nLinhas && i <= numberOfMovies; i++) { // acaba quando chegar ao maximo, seja ele o n de linhas ou o n de filmes pedidos
+			stock[i] = ler2.nextLine().split(",");
+		}
+		
+		ler2.close();
+		return stock;
+	}
 }
